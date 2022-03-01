e53bc5f625a0665101fa26a396a90655b5c97a5a
Chad Samuelson <crsamuelson87@gmail.com>
2022-03-01 10:48:59 -0700

    almost working prototype code

--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
 from scipy.optimize import minimize
 import numpy as np
+import matplotlib.pyplot as plt
 
 def obj(X,*args):
     X = X.reshape((int(len(X)/2),2))
@@ -11,15 +12,20 @@ def obj(X,*args):
 
 def anchor_con(X,*args):
     X = X.reshape((int(len(X)/2),2))
-    x0, step_size = args[0][0], args[0][1]
+    x0, step_size = args[0], args[1]
     constraint = []
     for i in range(np.size(X,0)):
         if i == 0:
             constraint.append(np.linalg.norm(X[i,:]-x0)-step_size)
         else:
             constraint.append(np.linalg.norm(X[i,:]-X[i-1,:])-step_size)
-    print(constraint)
     return np.array(constraint)
+
+def endReached(X,xf,tau):
+    for i in range(np.size(X,0)):
+        if np.linalg.norm(X[i,:] - xf) <= tau:
+            return True
+    return False
 
 if __name__ == '__main__':
     X = np.array([
@@ -32,16 +38,34 @@ if __name__ == '__main__':
     args = [xf]
 
     x0 = np.array([0,0])
-    step_size = 1.0
+    step_size = 0.5
     con_args = [x0,step_size]
-
     con = (
         {'type':'eq',
         'fun':anchor_con,
         'args':con_args}
     )
 
-    x = minimize(obj,X,args=args,constraints=con)
-    
-    print(x)
+    path = np.array([x0[:]])
+    tau = 1e-2
+    while not endReached(X,xf,tau):
+        x = minimize(obj,X,args=args,constraints=con)
+        X = x.x.reshape((int(len(x.x)/2),2))
+        path = np.vstack([path,X[0,:]])
 
+        x0 = X[0,:]
+        step_size = 1.0
+        con_args = [x0,step_size]
+        con = (
+            {'type':'eq',
+            'fun':anchor_con,
+            'args':con_args}
+        )
+        X = np.vstack([X[1:,:],X[-1,:]+1])
+    print(X)
+    print(path)
+    
+    plt.figure()
+    plt.plot(path)
+    plt.show()
+    
